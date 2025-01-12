54d956a0bd38feee22ec632bd3c3d12751fee221
RohitKumar773 <kumarrohit35511@gmail.com>
2025-01-12 20:28:14 +0530

    created table, profile update, dashboard, registration form,

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdminHomePageComponent } from './admin-home-page/admin-home-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegistrationsComponent } from './registrations/registrations.component';
 
 const routes: Routes = [
   { path: '', component: AdminLoginComponent },
-  { path: 'home', component: AdminHomePageComponent },
+  { path: 'admin_login', component: AdminLoginComponent },
   
+  {
+    path: 'home', component: AdminHomePageComponent,
+
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'registrations', component: RegistrationsComponent },
+    ],
+  },
 ];
 
 @NgModule({
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -12,9 +12,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ProfiledialogComponent } from './profiledialog/profiledialog.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegistrationsComponent } from './registrations/registrations.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 @NgModule({
-  declarations: [AdminLoginComponent, AdminHomePageComponent, ProfiledialogComponent],
+  declarations: [AdminLoginComponent, AdminHomePageComponent, ProfiledialogComponent, DashboardComponent, RegistrationsComponent, RegistrationFormComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,

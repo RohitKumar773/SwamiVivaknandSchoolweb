514fb1d83cd24fea62fd9c3d44c23290cd3c513f
RohitKumar773 <kumarrohit35511@gmail.com>
2025-02-08 22:14:07 +0530

    a

--- src/app/admin/course-applcation/course-applcation.component.ts
+++ src/app/admin/course-applcation/course-applcation.component.ts
@@ -9,6 +9,20 @@ import { ConfirmBoxComponent } from '../confirm-box/confirm-box.component';
   styleUrls: ['./course-applcation.component.scss'],
 })
 export class CourseApplcationComponent {
+   data = [
+    { id: 1, name: "Rohit Kumar", class: "LKG", location: "Hajipur" },
+    { id: 2, name: "Jacob", class: "Thornton", location: "raj" },
+    { id: 3, name: "Larry", class: "the Bird", location: "rajesh" },
+    { id: 4, name: "Mark", class: "Otto", location: "rohit" },
+    { id: 5, name: "Jacob", class: "Thornton", location: "raj" },
+    { id: 6, name: "Larry", class: "the Bird", location: "rajesh" },
+    { id: 7, name: "Mark", class: "Otto", location: "rohit" },
+    { id: 8, name: "Jacob", class: "Thornton", location: "raj" },
+    { id: 9, name: "Larry", class: "the Bird", location: "rajesh" },
+    { id: 10, name: "Mark", class: "Otto", location: "rohit" },
+    { id: 11, name: "Jacob", class: "Thornton", location: "raj" },
+    { id: 12, name: "Larry", class: "the Bird", location: "rajesh" }
+  ];
   constructor(private dialog: MatDialog) {}
 
   add_new_std() {

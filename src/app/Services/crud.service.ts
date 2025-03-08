4efd2f61e3f8d06bd32fdc6546fca62617508f02
Akhilesh <tech.kr.akhi@gmail.com>
2025-03-08 15:00:47 +0530

    correct it

--- src/app/Services/crud.service.ts
+++ src/app/Services/crud.service.ts
@@ -14,7 +14,7 @@ import { SubjectRes } from '../interface/subject.interface';
 })
 export class CrudService {
   // base_url: string = 'http://localhost/sawamivivekanand/'; //local
-  base_url: string = ' https://mausamstudio.com/Development/sawamivivekanand/ '; //Live
+  base_url: string = 'https://mausamstudio.com/Development/sawamivivekanand/ '; //Live
 
   constructor(private _http: HttpClient) { }
 

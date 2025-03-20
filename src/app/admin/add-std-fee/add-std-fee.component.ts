fff653bd98b313893f874c500ea5e2b3ae0366d5
RohitKumar773 <kumarrohit35511@gmail.com>
2025-03-21 00:32:57 +0530

    fee installment completed aprt from update

---
+++ src/app/admin/add-std-fee/add-std-fee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-std-fee',
+  templateUrl: './add-std-fee.component.html',
+  styleUrls: ['./add-std-fee.component.scss']
+})
+export class AddStdFeeComponent {
+
+}

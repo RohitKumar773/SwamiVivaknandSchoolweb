ca75b7b9af2d8a8b28eb4be5b9309139618613c8
Akhilesh <tech.kr.akhi@gmail.com>
2025-03-31 11:08:53 +0530

    chart

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { ChartOptions, ChartType, ChartData } from 'chart.js';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: true,
+  };
+
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+
+  public pieChartData: ChartData<'pie'> = {
+    labels: ['Male', 'Female'],
+    datasets: [
+      {
+        data: [60, 40], // Replace with actual student data
+        backgroundColor: ['#36A2EB', '#FF6384'],
+      },
+    ],
+  };
 
 }
